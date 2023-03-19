1919a9a35cab05b428cf9df9e5775a2c1f8ebd81
ChottoMatte <jin92370@gmail.com>
2023-03-20 01:43:18 +0900

    testコマンドを実装

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
+import 'dotenv/config';
+import { InteractionResponseType, InteractionType } from 'discord-interactions';
 import express from 'express';
+import { HasGuildCommands, TEST_COMMAND } from './commands.js';
+import { VerifyDiscordRequest } from './utils.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-// app.use(express.json({ verify: Ve}))
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+app.post('/interactions', async function(req, res) {
+    const { type, id, data } = req.body;
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+
+        if (name === 'test') {
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'hello world.'
+                }
+            });
+        }
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT);
+
+    HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+        TEST_COMMAND
+    ]);
+});
---
+++ commands.js
@@ -0,0 +1,43 @@
+import { DiscordRequest } from "./utils.js"
+
+export async function HasGuildCommands(appId, guildId, commands) {
+    if (guildId === '' || appId === '') return;
+
+    commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+}
+
+async function HasGuildCommand(appId, guildId, command) {
+    const endpoint = `applications/${appId}/guilds/${guildId}/commands`
+
+    try {
+        const res = await DiscordRequest(endpoint, { method: 'GET'});
+        const data = await res.json();
+
+        if (data) {
+            const installedNames = data.map((c) => c['name']);
+            if (!installedNames.includes(command['name'])) {
+                console.log(`Installing "${command['name']}"`);
+                InstallGuildCommand(appId, guildId, command);
+            } else {
+                console.log(`"${command['name']}" command already installed`);
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function InstallGuildCommand(appId, guildId, command) {
+    const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+    try {
+        await DiscordRequest(endpoint, { method: 'POST', body: command});
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const TEST_COMMAND = {
+    name: 'test',
+    description: 'Basic guild command',
+    type: 1,
+};
