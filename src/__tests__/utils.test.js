f26e0b54c7b31da91f98a4304b05ea7196b5388a
ChottoMatte <jin92370@gmail.com>
2023-03-29 21:42:53 +0900

    例外のテストを追加

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -13,5 +13,18 @@ describe('DiscordRequest', () => {
         const res = await DiscordRequest(endpoint, options);
         expect(fetchMock.calledOnce).to.equal(true);
         expect(res.ok).equals(true);
+        fetchMock.restore();
     });
+
+    it('it should be thrown exception when ok is not true', async () => {
+        const body = {json: () => ({error: "error message"})};
+        let fetchMock = await Sinon.stub(fetch, "Promise").resolves(body);
+        const endpoint = 'test/';
+        const options = {'test': 'test'};
+        await DiscordRequest(endpoint, options).catch((e) => {
+            expect(e.message).to.equal(JSON.stringify({error: "error message"}));
+        })
+        expect(fetchMock.calledOnce).to.equal(true);
+        fetchMock.restore();
+    })
 });
