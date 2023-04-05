303a14377a17bcd00f3685b1efb432cadf1b484e
ChottoMatte <jin92370@gmail.com>
2023-04-05 20:01:06 +0900

    test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,7 +1,8 @@
 import { expect } from "chai";
+import * as mockDI from "discord-interactions";
 import fetch from "node-fetch";
 import Sinon from "sinon";
-import { DiscordRequest } from "../utils.js";
+import { DiscordRequest, VerifyDiscordRequest } from "../utils.js";
 
 describe('DiscordRequest', () => {
     it('it should be success', async () => {
@@ -28,3 +29,16 @@ describe('DiscordRequest', () => {
         fetchMock.restore();
     })
 });
+
+describe('VerifyDiscordRequest', () => {
+    it('it should be success', () => {
+        const clientKey = 'testClientKey';
+        const verifyDiscordRequest = VerifyDiscordRequest(clientKey);
+        // Sinon.stub(verifyDiscordRequest).returns(true);
+        // let verifyKeyMock = Sinon.createStubInstance(VerifyDiscordRequest, {
+        //     verifyKey: Sinon.stub().returns(true)
+        // });
+        const mock = Sinon.stub(mockDI, 'verifyKey');
+        // mock("fefe", "feji", "fjeoiaw", "feaw").resolves(true);
+    })
+})
